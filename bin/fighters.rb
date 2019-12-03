e4f2d89cf7a592d1ff38780c3eb9f0af39308327
kasperken <kennethandrews@protonmail.com>
2019-12-03 17:21:07 -0500

    first commit

---
+++ bin/fighters.rb
@@ -0,0 +1,8 @@
+#BSD 3-Clause License
+
+#Copyright (c) 2019, Kenneth Andrews
+#All rights reserved.
+
+#!/bin/ruby
+
+require_relative 'environment.rb'
---
+++ gemfile.rb
@@ -0,0 +1,4 @@
+#BSD 3-Clause License
+
+#Copyright (c) 2019, Kenneth Andrews
+#All rights reserved.
